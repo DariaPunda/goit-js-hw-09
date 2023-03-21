ec378b25e2d75ab93255223294881e843626d3d9
DariaPunda <d.punda19@gmail.com>
2023-03-21 17:58:01 +0100

    1 task ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+btnStart.addEventListener('click', () => {
+    btnStart.disabled = true; 
+    btnStop.disabled = false; 
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+   
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+btnStop.addEventListener('click', () => {
+    btnStop.disabled = true; 
+    btnStart.disabled = false;
+    clearInterval(timerId);
+});
